b8cf0015811b8ee474a00f31f318cc131506b456
Keith <keith.dao.248@gmail.com>
2021-02-19 18:45:27 +1000

    Added structure for main function

--- chess.py
+++ chess.py
@@ -1,7 +1,31 @@
 import pygame
 
-def main():
+# Contants
+BOARD_WIDTH = 8
+BOARD_HEIGHT = 8
+
+# Functions
+def setup():
+    """
+    Setup the game board
+    """
     pass
+
+
+def game():
+    """
+    Play the game
+    """
+    gameOver = False
+    while not gameOver:
+        pass
+
+def main():
+    """
+    Setup and begin the game
+    """
+    setup()
+    game()
 
 if __name__ == "__main__":
     main()
