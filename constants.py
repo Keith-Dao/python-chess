7062b469addf7261f01ff8fcd7eaaefad7a8adda
Keith <keith.dao.248@gmail.com>
2021-02-21 11:41:19 +1000

    Added screen size constants

--- constants.py
+++ constants.py
@@ -1,11 +1,14 @@
 from enum import Enum
 
 # Board dimensions
-BOARD_WIDTH = 8
-BOARD_HEIGHT = 8
+BOARD_WIDTH = BOARD_HEIGHT = 8
 
 # Team colours
 class Colours(Enum):
     WHITE = 0
     BLACK = 1
 
+# Screen dimesions
+SCREEN_WIDTH = SCREEN_HEIGHT = 800 #pixels
+SQUARE_DIMENSIONS = 75 # pixels
+UI_WIDTH = UI_HEIGHT = 100 # pixels
