1d3fdb63f57399b13c8ffb00364be33edaabec72
Keith <keith.dao.248@gmail.com>
2021-02-27 17:58:33 +1000

    Added InvalidCoordinateException

--- exceptions.py
+++ exceptions.py
@@ -14,3 +14,20 @@ class EmptyCoordinateException(Exception):
             coord ((int, int)): The empty x- y-coordinate
         """
         self.message: str = f'The coordinate {coord} is empty.'
+
+class InvalidCoordinateException(Exception):
+    """
+    Exception raised for attempting to get an invalid coordinate.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, coord: (int, int)):
+        """
+        Initialise the empty coordinate exception.
+
+        Parameters:
+            coord ((int, int)): The invalid x, y position
+        """
+        self.message: str = f'There is no coordinate at (x,y): {coord}.'
