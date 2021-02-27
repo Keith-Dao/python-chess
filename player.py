0ab64d50089da4abd4b807be2ae85f2a231827ff
Keith <keith.dao.248@gmail.com>
2021-02-27 21:20:04 +1000

    Added bot classes

--- player.py
+++ player.py
@@ -1,3 +1,7 @@
+# Modules
+import random
+
+# Files
 from board import Board
 from pieces import Pieces
 
@@ -14,3 +18,40 @@ class Player(object):
         """
         self.colour: int = colour
         self.pieces: Pieces = Pieces(colour, board)
+
+
+class Bot(Player):
+    """ Generic bot. """
+    
+    def move(self):
+        """
+        Abstract method.
+        Generate a move.
+        """
+        pass
+
+
+class RandBot(Bot):
+    """ Bot that performs random moves. """
+
+    def move(self) -> tuple[tuple[int, int], tuple[int, int]]:
+        """ 
+        Randomly choose a possible move.
+
+        Returns:
+            tuple[tuple[int, int], tuple[int, int]]: Tuple of starting coordinates and ending coordinates
+        """
+        possibleMoves = self.pieces.getAllMoves()
+        return possibleMoves[random.randrange(0, len(possibleMoves))]
+
+
+class SmartBot(Bot):
+    """ Bot that strategically chooses moves. """
+    def move(self) -> tuple[tuple[int, int], tuple[int, int]]:
+        """
+        Strategically choose the next move.
+
+        Returns:
+            tuple[tuple[int, int], tuple[int, int]]: Tuple of starting coordinates and ending coordinates
+        """
+        pass
